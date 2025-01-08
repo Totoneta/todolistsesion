fcef7205170b7ce9e9a5edc248c1e0f2cfa91aac
Tomás Godoy <tomas.godoy.litio@hotmail.com>
2025-01-07 21:32:10 -0300

    Arreglo de errores

--- listproyect/settings.py
+++ listproyect/settings.py
@@ -110,7 +110,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#Heroku
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
